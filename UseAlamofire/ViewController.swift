b122f3e2e45e85bed758acb3eff23203ff7e98e7
codeliu6572 <codeliu6572@163.com>
2016-07-15 15:04:19 +0800

    Initial Commit

---
+++ UseAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UseAlamofire
+//
+//  Created by 刘浩浩 on 16/7/15.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
